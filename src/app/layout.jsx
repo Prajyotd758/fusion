8c169e9cc61ea9edccf4fa71fabe02ac736392ff
Prajyot Dange <prajyotdange32@gmail.com>
2026-06-30 16:34:34 +0530

    meta pixel issue solved

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,12 +23,12 @@ fbq('init', '1549225449992630');
 fbq('track', 'PageView');
 `,
           }}
-        ></script>
+        />
         <noscript>
           <img
             height="1"
             width="1"
-            style="display:none"
+            style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1549225449992630&ev=PageView&noscript=1"
           />
         </noscript>
